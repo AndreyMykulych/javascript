a9a8d8f375ab72327c83d7ae0dd1d74a74c41fae
Andriy Mykulych <aandriu201623@gmail.com>
2023-05-13 14:54:57 +0300

    15Homework

---
+++ 15/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    let imgArray = ['img/background1.png', 'img/girl.png', 'img/thirdcontent.png','img/firstphoto.png'];
+     
+        $('.block').width('1000px')
+        $('.first').attr('src', imgArray[0]);
+        $('.second').attr('src', imgArray[1]);
+        $('.third').attr('src', imgArray[2]);
+        $('.four').attr('src',imgArray[3])
+    $('.block').slick({
+        slidesToShow: 3,
+       
+    });
+    $(window).on('resize', () => {
+        let screenWidth = window.screen.width;
+        if (screenWidth < '500px') {
+            $('.block').slick({
+                slidesToShow: 1,
+            });
+        }
+       else  if (screenWidth < '1025px') {
+            $('.block').slick({
+                slidesToShow: 2,
+            });
+        }
+        else  {
+            $('.block').slick({
+                slidesToShow: 3,
+            });
+        }
+    })
+  });
